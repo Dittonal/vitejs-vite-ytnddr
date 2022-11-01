379072a9e8748ea829db2e0137a1426b75888243
Dittonal <53260392+Dittonal@users.noreply.github.com>
2022-11-01 14:04:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ytnddr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ytnddr)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+
+
+</template>
+
+<script>
+import requests from '../utils/requests';
+export default {
+  getData() {
+    request
+      .get('https://my-json-server.typicode.com/Dittonal/testapi/db')
+      .then((res) => {
+        console.log(res.data);
+      });
+  },
+};
+</script>
+
+<style></style>
